ced4eacc2e5c0aa45f5512ff448aa2cad21d0763
Atheesh Thirumalairajan <atheesh@atheesh-laptop.umd.edu>
2024-12-17 00:23:49 -0500

    Updated With Types, Other Improvements

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-const SpotifyPlugin: any = {
+import { IPlugin } from "@vortex-music/types/BasePlugin";
+
+
+const SpotifyPlugin: IPlugin = {
   id: 'vortexmusic-plugins-base',
   name: 'Base Template Plugin',
-  async initialize() {
-    console.log('Base Template Plugin initialized.');
+
+  player: {
+    play: function (): Promise<boolean> {
+      throw new Error("Function not implemented.");
+    },
+    pause: function (): Promise<boolean> {
+      throw new Error("Function not implemented.");
+    }
   },
-  
-  async play(trackId: string) {
-    console.log(`Playing track ${trackId} on Spotify.`);
-    return true;
+
+  description: "",
+  version: "",
+
+  activate: function (): Promise<boolean> {
+    throw new Error("Function not implemented.");
   },
+
+  deactivate: function (): Promise<boolean> {
+    throw new Error("Function not implemented.");
+  }
 };
 
 export default SpotifyPlugin;
